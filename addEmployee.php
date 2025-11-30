6cefa5f3391ea5ea5c2b023b744f0d54b6f71d1b
zubk36 <khanzubiyaa@gmail.com>
2025-11-30 16:13:44 -0600

    Created addEmployee.php

---
+++ addEmployee.php
@@ -0,0 +1,47 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "databasedarlings";
+$dbname = "art_museum";
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error){
+    die("Connection fail: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $fname = $_POST["fname"];
+    $minit = $_POST["minit"];
+    $lname = $_POST["lname"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $position = $_POST["position"];
+
+    $sql = "INSERT INTO employee (EFName, EMInitial, ELName, Phone, Email, Position)
+            VALUES (?, ?, ?, ?, ?, ?)";
+
+    $stmt = $conn->prepare($sql);
+
+    if (!$stmt) {
+        die("Prepare failed: " . $conn->error);
+    }
+
+    $stmt->bind_param("ssssss", $fname, $minit, $lname, $phone, $email, $position);
+
+    if ($stmt->execute()) {
+        echo "<h2>Employee added successfully!</h2>";
+        echo "<p><a href='addEmployee.html'>Add Another</a></p>";
+        echo "<p><a href='home.html'>Go Home</a></p>";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    $stmt->close();
+}
+
+$conn->close();
+
+?>
