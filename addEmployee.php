fe3f7f79190901554d75852e64414b99c8b192b1
Ananya-Sankaran <jayagurudattaladdu@gmail.com>
2025-12-02 22:53:49 -0600

    Update addEmployee.php

--- addEmployee.php
+++ addEmployee.php
@@ -20,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $phone = $_POST["phone"];
     $position = $_POST["position"];
 
-    $sql = "INSERT INTO employee (EFName, EMInitial, ELName, Phone, Email, Role)
+    $sql = "INSERT INTO employee (EFName, EMInitial, ELName, Phone, Email)
             VALUES (?, ?, ?, ?, ?, ?)";
 
     $stmt = $conn->prepare($sql);
