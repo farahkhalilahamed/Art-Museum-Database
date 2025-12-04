7aeed64f9513bd55fc2ba03f6b936c35b98e74c0
Disha <dishashetty99999@gmail.com>
2025-12-04 00:22:24 -0600

    Create updateExhibitionStaff.php

---
+++ updateExhibitionStaff.php
@@ -0,0 +1,66 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "databasedarlings";
+$dbname = "art_museum";
+
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error){
+  die("Connection fail:". $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"]=="POST")
+{
+  $fname = $_POST["fname"];
+  $minit = $_POST["minit"];
+  $lname = $_POST["lname"];
+  $email = $_POST["email"];
+  $phone = $_POST["phone"];
+  $employeeID = $_POST["employeeID"];
+  $sql = "UPDATE employee
+          SET EFName = ?,
+            EMInitial = ?,
+            ELName = ?,
+            Phone = ?, 
+            Email = ? 
+          WHERE EmployeeID = ?";
+
+  $stmt = $conn->prepare($sql);
+
+  if (!$stmt) {
+      die("Prepare failed: " . $conn->error);
+  }
+
+  $stmt->bind_param("sssisi", $fname, $minit, $lname, $phone, $email, $employeeID);
+
+  if ($stmt->execute()) {
+    $stmt->close();
+  }
+
+    $exhibitionID = intval($_POST["ExhibitionID"]);
+    $sql2 = "UPDATE ExhibitionStaff
+             SET EmployeeID = ?,
+             ExhibitionID = ?
+             WHERE EmployeeID = ?";
+    $stmt2 = $conn->prepare($sql2);
+    if (!$stmt2) {
+      die("Prepare failed: " . $conn->error);
+    }
+    $stmt2->bind_param("iii", $employeeID, $exhibitionID, $employeeID);
+      if ($stmt2->execute()) {
+        echo "<h2>Exhibition Staff updated successfully!</h2>";
+        echo "<p><a href='updateExhibitionStaff.html'>Add Another</a></p>";
+        echo "<p><a href='home.html'>Go Home</a></p>";
+    } else {
+        echo "Error updating exhibition staff: " . $stmt2->error;
+    }
+
+    $stmt2->close();
+    }else {
+        echo "Error updating employee: " . $stmt->error;
+    }
+$conn->close();
+?>
