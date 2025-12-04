2a2526180060fe3e849ad899d5a5969787ef523d
Disha <dishashetty99999@gmail.com>
2025-12-04 17:16:05 -0600

    Create addArtistBad.php

---
+++ addArtistBad.php
@@ -0,0 +1,49 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "databasedarlings";
+$dbname = "art_museum";
+
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error){
+  die("Connection fail:". $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"]=="POST")
+{
+  $fname=$_POST["afname"];
+  $minit=$_POST["aminitial"];
+  $lname=$_POST["alname"];
+  $dob=$_POST["dob"];
+  $formattedDOB = date("Ymd", strtotime($dob));
+
+	
+  $country=$_POST["country"];
+  $bio=$_POST["bio"];
+
+  $sql = "INSERT INTO artist (AFName, AMInitial, ALName, DOB, Country, Bio)
+          VALUES ($fname, $minit, $lname, $formattedDOB, $country, $bio)";
+  $stmt = $conn->prepare($sql);
+
+    if (!$stmt) {
+        die("Prepare failed: " . $conn->error);
+    }
+  #$stmt->bind_param("ssssss", $fname, $minit, $lname, $formattedDOB, $country, $bio);
+  if ($stmt->execute()) {
+        echo "<h2>Artist added successfully!</h2>";
+        echo "<p><a href='addArtist.html'>Add Another</a></p>";
+        echo "<p><a href='home.html'>Go Home</a></p>";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    $stmt->close();
+}
+
+$conn->close();
+?>
+
+  
