b8d16605a00d61dd17d0027cc71d939a4c238aa5
Disha <dishashetty99999@gmail.com>
2025-12-03 20:24:04 -0600

    Update addExhibition.php

--- addExhibition.php
+++ addExhibition.php
@@ -40,34 +40,37 @@ if (isset($_POST['submit']))
 
     $stmt->close();
 }
-if (!empty($artwork) && is_array($artworks){
-  // if (trim($artworkTitle) == "") continue;
-  $search = "%{$artwork}%";
-  $stmt = $conn->prepare("SELECT ArtworkID, ArtistID
-                          FROM artwork 
-                          WHERE Title Like ?");
-  $stmt->bind_param("s", $search);
-  $stmt->execute();
-  $result = $stmt->get_result();
+if (!empty($artwork) && is_array($artwork)){
+  foreach ($artwork as $artworkTitle) {
+    $artworkTitle = trim($artworkTitle);
+    
+    if ($artworkTitle == "") continue;
+    $search = "%{$artworkTitle}%";
+    $stmt = $conn->prepare("SELECT ArtworkID, ArtistID
+                            FROM artwork 
+                            WHERE Title Like ?");
+    $stmt->bind_param("s", $search);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
 
 
-  if ($result->num_rows > 0){
-    while ($row = $result->fetch_assoc()) 
-    {
-      $artworkID= $row['ArtworkID'];
-      $artistID= $row['ArtistID'];
-      $stmt2 = $conn->prepare("INSERT INTO ExhibitedArtwork (ExhibitionID, ArtworkID, ArtistID) 
-                              VALUES (?,?,?)");
-      $stmt2->bind_param("iii", $exhibitionID, $artworkID, $artistID);
-      $stmt2->execute();
-      $stmt2->close();
+    if ($result->num_rows > 0){
+      while ($row = $result->fetch_assoc()) 
+      {
+        $artworkID= $row['ArtworkID'];
+        $artistID= $row['ArtistID'];
+        $stmt2 = $conn->prepare("INSERT INTO ExhibitedArtwork (ExhibitionID, ArtworkID, ArtistID) 
+                                VALUES (?,?,?)");
+        $stmt2->bind_param("iii", $exhibitionID, $artworkID, $artistID);
+        $stmt2->execute();
+        $stmt2->close();
+      }
       echo "<h2>Artwork added successfully!</h2>"; 
-    }}
-  else
-    echo "<h2>No artwork found :( </h2>";
-  $stmt->close();
-}
+    }else
+    {echo "<h2>No artwork found :( </h2>";}
+    $stmt->close();
+}}
 echo "<p><a href='addExhibition.html'>Add Another</a></p>";
 echo "<p><a href='home.html'>Go Home</a></p>";
 $conn->close();
